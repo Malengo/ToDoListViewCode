458e9b5732c5034fc0e8787dd7e5a5eb597f627e
JMalengo <jose.malengo@hotmail.com>
2022-06-23 17:28:26 -0300

    setting contrainsts all components from mainView

--- TodoListViewCode/View/MainView.swift
+++ TodoListViewCode/View/MainView.swift
@@ -9,29 +9,58 @@ import UIKit
 
 class MainView: UIView {
     
+    //MARK: -- View Components
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView(frame: .zero)
+        stackView.distribution = .fill
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        stackView.alignment = .center
+        return stackView
+    }()
+    
     //MARK: -- components
     lazy var coreDataButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("ToDo using CoreData", for: .normal)
-        button.setTitleColor(.black, for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 8
         return button
     }()
     
     lazy var realmButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("ToDo using Realm", for: .normal)
-        button.backgroundColor = .systemRed
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 8
         return button
     }()
+    
     //MARK: -- Setup components
     func setupView() {
         backgroundColor = .white
-        addSubview(coreDataButton)
+        addSubview(stackView)
+        [coreDataButton,
+         realmButton].forEach { stackView.addArrangedSubview($0) }
     }
     
     func setupConstraints() {
-        coreDataButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-        coreDataButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        stackView.leftAnchor.constraint(equalTo: leftAnchor, constant: 10).isActive = true
+        stackView.rightAnchor.constraint(equalTo: rightAnchor, constant: -10).isActive = true
+        
+        realmButton.translatesAutoresizingMaskIntoConstraints = false
+        realmButton.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        realmButton.leftAnchor.constraint(equalTo: stackView.leftAnchor).isActive = true
+        realmButton.rightAnchor.constraint(equalTo: stackView.rightAnchor).isActive = true
+        
         coreDataButton.translatesAutoresizingMaskIntoConstraints = false
+        coreDataButton.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        coreDataButton.leftAnchor.constraint(equalTo: stackView.leftAnchor).isActive = true
+        coreDataButton.rightAnchor.constraint(equalTo: stackView.rightAnchor).isActive = true
     }
 }
